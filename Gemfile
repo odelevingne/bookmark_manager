747ce621d325fbf4cc6c8ce78eb65b32b2289847
Oliver Delevingne <oliver@delevingne.co.uk>
2014-07-30 21:33:24 +0100

    users can be created

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://www.rubygems.org/"
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'bcrypt-ruby'
+
 
 group :development, :test do
   
---
+++ app/server.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require 'data_mapper'
+
+require './lib/link' 
+require './lib/tag'
+require './lib/user'
+
+require_relative 'data_mapper_setup'
+
+class BookmarkManager < Sinatra::Base
+
+	enable :sessions
+	set :session_secret, 'super secret'
+
+	get '/' do
+		@links = Link.all
+		erb :index
+	end
+
+	post '/links' do
+	  url = params["url"]
+	  title = params["title"]
+	  tags = params["tags"].split(" ").map do |tag|
+	  	Tag.first_or_create(:text => tag)
+	  end
+	  Link.create(:url => url, :title => title, :tags => tags)
+	  redirect to('/')
+	end
+
+	get '/tags/:text' do
+		tag =Tag.first(:text => params[:text])
+		@links = tag ? tag.links : []
+		erb :index
+	end
+
+	get '/users/new' do
+		erb :"users/new"
+	end
+
+	post '/users' do
+		user = User.create(:email => params[:email],
+								:password => params[:password])
+		session[:user_id] = user.id
+		redirect to ('/')
+	end
+
+	helpers do
+	  def current_user    
+	    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+	  end
+	end
+end
