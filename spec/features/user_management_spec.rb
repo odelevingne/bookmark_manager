11dc9468d08d4afb82d6457fb9a2b72fffed593b
Oliver Delevingne <oliver@delevingne.co.uk>
2014-07-31 12:30:58 +0100

    password confirmation now required

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -9,15 +9,19 @@ feature "User signs up" do
 	end
 
 	def sign_up(email = "alice@example.com",
-							password = "oranges!")
+							password = "oranges!",
+							password_confirmation = "oranges!")
 		visit '/users/new'
 		expect(page.status_code).to eq(200)
 		expect(page.status_code).to eq(200)
 		fill_in :email, :with => email
 		fill_in :password, :with => password
+		fill_in :password_confirmation, :with => password_confirmation
 		click_button "Sign Up"
 	end
 
-
+	scenario "with a password that doesn't match" do
+		lambda { sign_up(email = 'a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)
+	end
 
 end
